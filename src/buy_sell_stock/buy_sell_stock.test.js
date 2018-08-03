3414a6d5c789c62d37277c4a8d879eaf44a3e202
Steven Magelowitz <steven.magelowitz@gmail.com>
2018-08-02 22:59:05 -0700

    format

--- src/buy_sell_stock/buy_sell_stock.test.js
+++ src/buy_sell_stock/buy_sell_stock.test.js
@@ -1,9 +1,9 @@
 import maxProfit from './buy_sell_stock';
 
 test('returns the max profit for the given list of prices', () => {
-  expect(maxProfit([7,1,5,3,6,4])).toEqual(5);
+  expect(maxProfit([7, 1, 5, 3, 6, 4])).toEqual(5);
 });
 
 test('returns the max profit for the given list of prices', () => {
-  expect(maxProfit([7,6,4,3,1])).toEqual(0);
+  expect(maxProfit([7, 6, 4, 3, 1])).toEqual(0);
 });
