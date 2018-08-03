907b8af102004ae7f10af3803e2dcb3ff6e1072d
Steven Magelowitz <steven.magelowitz@gmail.com>
2018-08-02 22:44:08 -0700

    fix test

--- src/two_sum/two_sum.test.js
+++ src/two_sum/two_sum.test.js
@@ -1,5 +1,9 @@
 import twoSum from './two_sum';
 
-test('adds 1 + 2 to equal 3', () => {
+test('returns the first two indices that add up to the sum', () => {
   expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
+
+test('returns the first two indices that add up to the sum', () => {
+  expect(twoSum([8, 7, 2, 15], 9)).toEqual([1, 2]);
 });
